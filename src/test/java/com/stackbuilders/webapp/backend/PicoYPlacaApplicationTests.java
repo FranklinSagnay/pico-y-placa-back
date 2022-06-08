62228348d8afed6e2296b208e08f456bba4e0ffb
Franklin Sagnay <danilo.sagnay98@hotmail.com>
2022-06-07 23:44:54 -0500

    Initial springboot configuration

---
+++ src/test/java/com/stackbuilders/webapp/backend/PicoYPlacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackbuilders.webapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoYPlacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
